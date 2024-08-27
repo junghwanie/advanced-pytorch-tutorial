4e7465521b0ff26e0b443a7d35337820abb3b2b4
junghwanie <edd0718@gmail.com>
2024-08-27 14:33:35 +0900

    typo

--- tutorials/advanced/variational_autoencoder/main.py
+++ tutorials/advanced/variational_autoencoder/main.py
@@ -47,4 +47,53 @@ class VAE(nn.Module):
     def encode(self, x):
         h = F.relu(self.fc1(x))
         return self.fc2(h), self.fc3(h)
-    
+
+    def reparameterize(self, mu, log_var):
+        std = torch.exp(log_var/2)
+        eps = torch.randn_like(std)
+        return mu + eps * std
+
+    def decode(self, z):
+        h = F.relu(self.fc4(z))
+        return F.sigmoid(self.fc5(h))
+
+    def forward(self, x):
+        mu, log_var = self.encode(x)
+        z = self.reparameterize(mu, log_var)
+        x_reconst = self.decode(z)
+        return x_reconst, mu, log_var
+
+model = VAE().to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Start training
+for epoch in range(num_epochs):
+    for i, (x, _) in enumerate(data_loader):
+        # Forward pass
+        x = x.to(device).view(-1, image_size)
+        x_reconst, mu, log_var = model(x)
+
+        # Compute reconstruction loss and kl divergenece
+        reconst_loss = F.binary_cross_entropy(x_reconst, x, size_average=False)
+        kl_div = - 0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
+
+        # Backprop and optimize
+        loss = reconst_loss + kl_div
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i+1) % 10 == 0:
+            print ("Epoch[{}/{}], Step [{}/{}], Reconst Loss: {:.4f}, KL Div: {:.4f}" 
+                   .format(epoch+1, num_epochs, i+1, len(data_loader), reconst_loss.item(), kl_div.item()))
+
+    with torch.no_grad():
+        # Save the sampled images
+        z = torch.randn(batch_size, z_dim).to(device)
+        out = model.decode(z).view(-1, 1, 28, 28)
+        save_image(out, os.path.join(sample_dir, 'sampled-{}.png'.format(epoch+1)))
+
+        # Save the reconstructed images
+        out, _, _ = model(x)
+        x_concat = torch.cat([x.view(-1, 1, 28, 28), out.view(-1, 1, 28, 28)], dim=3)
+        save_image(x_concat, os.path.join(sample_dir, 'reconst-{}.png'.format(epoch+1)))
